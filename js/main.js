8527e16d7dab920c9ce93bc1a48267c49a834a7a
davincikab <davidnganganjer079@gmail.com>
2019-04-26 11:03:11 +0300

    load a shapefile to the map container

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ let map = L.map('map').setView([0.9889, 37.987], 12);
 
 // Add a basemap
 let esristreet = L.esri.basemapLayer('Streets').addTo(map);
+
+var shpfile = new L.Shapefile('data.zip');
+shpfile.addTo(map);
+
+map.fitBounds(shpfile.getBounds());
